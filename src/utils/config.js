c11c966ec98f9e6adde9d79d46b05abfe39c5481
KeldiyorDev <keldiyorraxmatov1303@gmail.com>
2024-11-19 19:39:24 +0500

    config

--- src/utils/config.js
+++ src/utils/config.js
@@ -19,7 +19,7 @@ axiosInstance.interceptors.request.use(function (config) {
 
 export default axiosInstance;
 
-baseURL: "https://95.46.96.154:5010/api",
+export const url = "https://95.46.96.154:5010/api"
 // export const url = "http://localhost:9090"
 // export const url = "http://api.edureyting.uz/api"
 // export const url = "https://95.47.127.171:5010/api"
