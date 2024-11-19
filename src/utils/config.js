0bde0d42a84a3cd5d8509b4f69ee02f1de300486
KeldiyorDev <keldiyorraxmatov1303@gmail.com>
2024-11-19 19:37:15 +0500

    port 4100

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const defaultOptions = {
+    baseURL: "https://95.46.96.154:5010/api",
     // baseURL: "http://localhost:9090",
-     baseURL: "http://api.edureyting.uz/api",
+    //  baseURL: "http://api.edureyting.uz/api",
     //baseURL: "https://95.47.127.171:5010/api",
     headers: {
         'Content-Type': 'application/json',
@@ -18,6 +19,7 @@ axiosInstance.interceptors.request.use(function (config) {
 
 export default axiosInstance;
 
+baseURL: "https://95.46.96.154:5010/api",
 // export const url = "http://localhost:9090"
-export const url = "http://api.edureyting.uz/api"
+// export const url = "http://api.edureyting.uz/api"
 // export const url = "https://95.47.127.171:5010/api"
